6a21729d62663d632babcf57b70cd374fa9dc6e5
richo <richo@psych0tik.net>
2012-12-08 23:27:52 +1100

    Refactor a ton of magic out, doesn't do branching search

--- main.scm
+++ main.scm
@@ -11,26 +11,31 @@
 (define word-list
   (read-lines word-list-file))
 
-
-(define real-find-matches
-  (lambda (exit tokens current-token)
-    (let* ((first (string-ref current-token 0))
-           (new-tokens (append tokens (list first))))
-      (cond ((= (string-length current-token) 0) (exit (list new-tokens "")))
-            (else (real-find-matches exit new-tokens (substring current-token 1 (string-length current-token)))))
-    )))
-
 (define find-matches
-  (lambda (token)
-    (let ((process (lambda (current-values)
-      (let* ((_tmp (call-with-current-continuation (lambda (cc)
-                                                     (real-find-matches cc (car current-values) (cdr current-values)))))
-             (tokens (car _tmp))
-             (remaining (cdr _tmp)))
-        (cond ((= (string-length remaining) 0) tokens)
-              (else (process current-values)))))))
-      (process (list '() token)))))
+  (lambda (token return)
+    (define process-token (lambda (tokens remaining)
+                     (map (lambda (word)
+                            (if (string=? word remaining)
+                                      (return (append tokens (list word))))
+                            (if (string-prefix? word remaining)
+                                    (process-token (append tokens (list word))
+                                             (substring remaining (string-length word) (string-length remaining))))
+                            )
+                          word-list)))
+    (process-token (list) token)
+    ;; Catch the case where we don't find anything return an empty list
+    '()
+    ))
+
+
 
 (define main
   (lambda (argv)
-    (map (lambda (token) (display (string-append (find-matches token) "\n"))) argv)))
+    (map
+      (lambda (token)
+        (display (string-intersperse
+                   (call-with-current-continuation (lambda (cc) (find-matches token cc)))
+                   "|"
+                   ))
+        (display "\n"))
+        argv)))
