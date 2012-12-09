bca192298929a71347196eb4f02c119968fdb009
richo <richo@psych0tik.net>
2012-12-10 00:13:12 +1100

    More hash-table refactoring

--- main.scm
+++ main.scm
@@ -15,9 +15,7 @@
   (with-input-from-file word-list-file
     (lambda () (port-fold (lambda (line hash)
                             (let* ((first-two (substring line 0 hash-table-depth)))
-                              (if (hash-table-exists? hash first-two)
-                                  (hash-table-set! hash first-two (append (hash-table-ref hash first-two) (list line)))
-                                  (hash-table-set! hash first-two (list line))))
+                              (hash-table-set! hash first-two (append (hash-table-ref/default hash first-two (list)) (list line))))
                             hash)
                           (make-hash-table)
                           read-line))))
