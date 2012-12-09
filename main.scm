68129caa9babb8b11bd9022e300f41adab77400d
richo <richo@psych0tik.net>
2012-12-09 19:49:00 +1100

    Start port to hash-tables over a big list of words

--- main.scm
+++ main.scm
@@ -2,6 +2,9 @@
 
 (use utils)
 (use srfi-13)
+(use srfi-69)
+
+(define hash-table-depth 2)
 
 (define word-list-file
   (let ((environment-file (get-environment-variable "WORDLIST_FILE")))
@@ -9,7 +12,14 @@
           (else "/usr/share/dict/words"))))
 ; Read all words into memory
 (define word-list
-  (read-lines word-list-file))
+  (with-input-from-file word-list-file
+    (lambda () (port-fold (lambda (line hash)
+                            (let* ((first-two (substring line 0 1)))
+                              (if (hash-table-exists? hash first-two)
+                                  (hash-table-set! hash first-two (append (hash-table-ref hash first-two) (list line)))
+                                  (hash-table-set! hash first-two (list line)))))
+      (make-hash-table)
+      read-line))))
 
 (define find-matches
   (lambda (token return)
